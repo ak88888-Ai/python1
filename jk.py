33a76877a4071ef47326bf85a2b2036e22f7576c
ak88888-Ai <1500713912@qq.com>
2025-11-17 22:35:56 +0800

    Update jk.py

--- jk.py
+++ jk.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 # 定义一个GET接口，路径为/api/hello
 @app.route("/api/hello", methods=["GET"])
 def hello():
-    return jsonify({"status": "success", "message": "Hello from server!"})
+    return jsonify({"status": "success", "message": "万富豪你看看可以不!"})
 
 if __name__ == "__main__":
     # 关键：host=0.0.0.0允许外部访问，port指定端口（如5000）
+
     app.run(host="0.0.0.0", port=5000, debug=False)  # 生产环境关闭debug
